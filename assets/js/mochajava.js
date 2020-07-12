eab707544b8072277ea377773671a9eeba17b8fc
Jacob Daughtry <akutozo@gmail.com>
2020-07-12 15:20:31 -0500

    action plan - step 1

---
+++ assets/js/mochajava.js
@@ -0,0 +1,20 @@
+
+
+
+const apikey = "f431e0b2fba2d152c610f1222f98d6d3";
+
+
+//build a function that calls the API key to pull weather data.
+//Example: https://api.openweathermap.org/data/2.5/weather?q=Chicago&appid=f431e0b2fba2d152c610f1222f98d6d3
+//Tie that function to the html document's search-button
+
+//Items needed WILL include:
+// 
+// City Name
+// Temperature
+// Humidity
+// UV Index
+// Wind
+// 5 Day Forecast
+// 
+// WHEN a search is complete, commit the search to local storage
