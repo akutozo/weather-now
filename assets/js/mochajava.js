16dbf619b68431f0c714d41f87753b2cd5bdacaf
Jacob Daughtry <akutozo@gmail.com>
2020-07-12 19:19:27 -0500

    Successful EventLister for city

--- assets/js/mochajava.js
+++ assets/js/mochajava.js
@@ -1,5 +1,5 @@
 // Determine the city
-const cityEl = document.getElementById("city-name-input");
+var cityInputEl = document.querySelector("#city-name-input");
 // apikey to authorize search
 const apikey = "f431e0b2fba2d152c610f1222f98d6d3";
 // execute search
@@ -7,7 +7,7 @@ const searchbtnEl = document.getElementById("search-button");
 
 
 //build a function that calls the API key to pull weather data. -Function added, citysearch
-//Example: https://api.openweathermap.org/data/2.5/weather?q=Chicago&appid=f431e0b2fba2d152c610f1222f98d6d3 -Function working
+//https://api.openweathermap.org/data/2.5/weather?q=Chicago&appid=f431e0b2fba2d152c610f1222f98d6d3 -Function working
 //Tie that function to the html document's search-button
 
 //Items needed WILL include:
@@ -23,8 +23,8 @@ const searchbtnEl = document.getElementById("search-button");
 
 
 
-var cityweather = function () {
-    var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + cityEl + "&appid=" + apikey;
+var cityweather = function (search) {
+    var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + search + "&appid=" + apikey;
     console.log(apiUrl);
 
     fetch(apiUrl).then(function (response) {
@@ -37,14 +37,13 @@ var cityweather = function () {
     });
 };
 
-cityweather();
+// cityweather();
 
-//Let's see if our button is clinking
+//Take the city and push it through cityweather
 searchbtnEl.addEventListener("click",function() {
-    console.log("I've been clicked!");
-    // const searchTerm = cityEl.value;
-    // getWeather(searchTerm);
-    // searchHistory.push(searchTerm);
-    // localStorage.setItem("search",JSON.stringify(searchHistory));
-    // renderSearchHistory();
+    var cityInput = cityInputEl.value.trim();
+    console.log("Searching for weather in: " + cityInput);
+    const search = cityInput;
+
+    cityweather(search);
 })
