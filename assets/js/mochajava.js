0dd39138d2460e7b810daf44876910f0cf1e23a1
Jacob Daughtry <akutozo@gmail.com>
2020-07-12 22:16:27 -0500

    Successfully added temp, humidity, wind speed

--- assets/js/mochajava.js
+++ assets/js/mochajava.js
@@ -4,13 +4,15 @@ var cityInputEl = document.querySelector("#city-name-input");
 const apikey = "f431e0b2fba2d152c610f1222f98d6d3";
 // execute search
 const searchbtnEl = document.getElementById("search-button");
+//current weather display
+const citynameEl = document.getElementById("city-name");
+const cImageEl = document.getElementById("current-image");
+const cTempEl = document.getElementById("temp");
+const cHumidityEl = document.getElementById("humidity");4
+const cWindEl = document.getElementById("wind");
+const cUVEl = document.getElementById("UV");
 
-
-//build a function that calls the API key to pull weather data. -Function added, citysearch
-//https://api.openweathermap.org/data/2.5/weather?q=Chicago&appid=f431e0b2fba2d152c610f1222f98d6d3 -Function working
-//Tie that function to the html document's search-button
-
-//Items needed WILL include:
+//Capture the following:
 // 
 // City Name
 // Temperature
@@ -24,20 +26,27 @@ const searchbtnEl = document.getElementById("search-button");
 
 
 var cityweather = function (search) {
-    var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + search + "&appid=" + apikey;
-    console.log(apiUrl);
-
-    fetch(apiUrl).then(function (response) {
-        if (response.ok) {
-            response.json().then(function (data) {
+    let apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + search + "&appid=" + apikey;
+    console.log(apiUrl)
+    fetch(apiUrl)
+    .then(function (response) {
+            response.json().then(function(data) {
+                console.log(data);
+                const currentDate = new Date(data.dt*1000);
+                console.log(currentDate);
+                const day = currentDate.getDate();
+                const month = currentDate.getMonth() + 1;
+                const year = currentDate.getFullYear();
+                citynameEl.innerHTML = data.name + " (" + month + "/" + day + "/" + year + ") ";
+                let weatherImage = data.weather[0].icon;
+                cImageEl.setAttribute("src","https://openweathermap.org/img/wn/" + weatherImage + "@2x.png");
+                cImageEl.setAttribute("alt",data.weather[0].description);
+                cTempEl.innerHTML = "Current Temperature: " + data.main.temp + " &#176F";
+                cHumidityEl.innerHTML = "Current Humidity: " + data.main.humidity + "%";
+                cWindEl.innerHTML = "Current Wind Speed: " + data.wind.speed + " MPH";
             });
-        } else {
-            alert("Error: " + response.statusText);
-        }
     });
 };
-
-// cityweather();
 
 //Take the city and push it through cityweather
 searchbtnEl.addEventListener("click",function() {
